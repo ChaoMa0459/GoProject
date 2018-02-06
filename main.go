a208e18cd144575aeea1ed75ba1fe6590a6892eb
SwagMC <mc163@bu.edu>
2018-02-05 22:33:33 -0500

    changed URL

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.152.174:9200/"
+	ES_URL = "http://35.227.124.24:9200/"
 )
 
 
