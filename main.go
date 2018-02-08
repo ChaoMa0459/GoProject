4221b74d91632669c79b3cb0b64e8ed07d91486e
Jiachen Xiao <luvianchen@JiachendeMacBook-Air.local>
2018-02-07 20:47:51 -0500

    run a test of different ES_URL

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.227.124.24:9200/"
+	ES_URL = "http://35.231.80.20:9200/"
 )
 
 
